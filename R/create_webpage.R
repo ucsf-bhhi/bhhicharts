58855e6ecb2e63df1a8c914f5af0fd28464d7e93
Eve Perry <eve@eveperry.com>
2023-03-16 16:14:53 -0700

    create_webpage() updates
    add root_dir argument
    set default page_title argument
    refactor initial chart picking
    allow for multiple charts with no supplied picker text
    always set initial picker option to initial chart

--- R/create_webpage.R
+++ R/create_webpage.R
@@ -22,40 +22,47 @@
 #'   c("Chart 1", "Chart 2", "Chart 3")
 #' )
 #'
-create_webpage = function(chart_objects, path, page_title, initial_chart = NULL, chart_picker_text = NULL, view = TRUE) {
-  if (length(chart_objects) == 1 | highcharter::is.highchart(chart_objects)) {
-    initial_chart = chart_objects
-  } else {
-    initial_chart = chart_objects[[initial_chart]]
-  }
-
+create_webpage = function(
+    chart_objects, html_filename, root_dir = ".", page_title = "Chart",
+    initial_chart = NULL, chart_picker_text = NULL, view = TRUE
+) {
   chart_picker = FALSE
-  if (
-    !is.null(chart_picker_text) &
-    length(chart_objects) > 1 &
-    !highcharter::is.highchart(chart_objects)
-  ) {
+  if (length(chart_objects) > 1 & !highcharter::is.highchart(chart_objects)) {
+    chart_picker = TRUE
+
+    if (is.null(chart_picker_text))
+      chart_picker_text = names(chart_objects)
+
     chart_picker_text = purrr::map2(
       names(chart_objects), chart_picker_text,
-      ~ list(value = .x, text = .y)
+      function(value, text) {
+        selected = ifelse(
+          !is.null(initial_chart) && value == initial_chart, "selected", ""
+        )
+        list(value = glue::glue("value='{value}' {selected}"), text = text)
+      }
     )
-    chart_picker = TRUE
   }
 
   chart_data_path = NULL
   if (length(chart_objects) > 1 & !highcharter::is.highchart(chart_objects)) {
-    fs::dir_create("chart-data")
+    fs::dir_create(root_dir, "chart-data")
 
     chart_data_path = fs::path(
-      "chart-data", paste0(fs::path_ext_remove(path), "_data"), ext = "js"
+      root_dir, "chart-data", paste0(fs::path_ext_remove(html_filename), "_data"),
+      ext = "js"
     )
 
     export_chart_data(chart_objects, chart_data_path)
+
+    chart_data_path = build_read_path_rel(root_dir, chart_data_path)
   }
 
-  add_dependency("css", "styles.css")
-  add_dependency("js", "bhhiTheme.js")
-  add_dependency("img", "bhhi_logo_lean.png")
+  add_dependency(root_dir, "css", "styles.css")
+  add_dependency(root_dir, "js", "bhhiTheme.js")
+  add_dependency(root_dir, "img", "bhhi_logo_lean.png")
+
+  html_path = fs::path(root_dir, html_filename)
 
   whisker::whisker.render(
     readLines(
@@ -63,21 +70,30 @@ create_webpage = function(chart_objects, path, page_title, initial_chart = NULL,
     ),
     list(
       page_title = page_title,
-      initial_chart = jsonlite::toJSON(
-        initial_chart[["x"]][["hc_opts"]],
-        auto_unbox = TRUE,
-        null = "null",
-        na = "null"
-      ),
+      initial_chart = initial_chart_json(chart_objects, initial_chart),
       chart_picker = chart_picker,
       chart_picker_text = chart_picker_text,
       chart_data = chart_data_path
     )
   ) |>
-    writeLines(path)
+    writeLines(html_path)
 
   if (view & interactive())
-    fs::file_show(path)
+    fs::file_show(html_path)
+}
+
+initial_chart_json = function(chart_objects, initial_chart) {
+  if (length(chart_objects) == 1 | highcharter::is.highchart(chart_objects)) {
+    initial_chart = chart_objects
+  } else {
+    if (is.null(initial_chart)) {
+      initial_chart = chart_objects[[1]]
+    } else {
+      initial_chart = chart_objects[[initial_chart]]
+    }
+  }
+
+  chart_to_json(initial_chart)
 }
 
 #' Add HTML dependency
@@ -89,17 +105,28 @@ create_webpage = function(chart_objects, path, page_title, initial_chart = NULL,
 #'
 #' @examplesIf interactive()
 #' add_dependency("css", "styles.css")
-add_dependency = function(...) {
-  path = fs::path(...)
+add_dependency = function(root_dir, ...) {
+  write_path = fs::path(root_dir, ...)
 
-  if (!fs::file_exists(path)) {
-    fs::dir_create(fs::path_dir(path))
+  if (!fs::file_exists(write_path)) {
+    fs::dir_create(fs::path_dir(write_path))
+
+    read_path_rel = build_read_path_rel(root_dir, write_path)
 
     fs::file_copy(
-      fs::path_package("bhhicharts", "inst", path),
-      path
+      fs::path_package("bhhicharts", "inst", read_path_rel),
+      write_path
     )
   }
+}
+
+build_read_path_rel = function(root_dir, write_path) {
+  write_path_components = fs::path_split(write_path)[[1]]
+  root_path_components = fs::path_split(root_dir)[[1]]
+  read_path_components =
+    write_path_components[!(write_path_components %in% root_path_components)]
+
+  fs::path_join(read_path_components)
 }
 
 is.highchartzero = function(x) {
