e953b497929c6fd190243a30240d74ffedb08c2f
Eve Perry <eve@eveperry.com>
2023-03-16 16:09:24 -0700

    clean up test fixtures

--- tests/testthat/test-chart_to_json.R
+++ tests/testthat/test-chart_to_json.R
@@ -1,6 +1,4 @@
-test_chart_to_json = function(x) {
-  path = tempfile(fileext = ".json")
-
+test_chart_to_json = function(x, path) {
   chart_to_json(x) |>
     writeLines(path)
 
@@ -8,19 +6,25 @@ test_chart_to_json = function(x) {
 }
 
 test_that("converting single chart to json works", {
-  data(example_charts)
+  data(example_charts, envir = rlang::current_env())
 
-  expect_snapshot_file(
-    test_chart_to_json(example_charts[[1]]),
-    "single_chart.json"
+  withr::local_file(
+    "test.json",
+    expect_snapshot_file(
+      test_chart_to_json(example_charts[[1]], "test.json"),
+      "single_chart.json"
+    )
   )
 })
 
 test_that("converting multiple charts to json works", {
-  data(example_charts)
+  data(example_charts, envir = rlang::current_env())
 
-  expect_snapshot_file(
-    test_chart_to_json(example_charts),
-    "multiple_charts.json"
+  withr::local_file(
+    "test.json",
+    expect_snapshot_file(
+      test_chart_to_json(example_charts, "test.json"),
+      "multiple_charts.json"
+    )
   )
 })
