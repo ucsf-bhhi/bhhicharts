9d1d71169d296faed72621671fadc731a5a800e3
Eve Perry <eve@eveperry.com>
2023-03-17 15:52:03 -0700

    refactor chart_to_json

--- R/chart_to_json.R
+++ R/chart_to_json.R
@@ -23,11 +23,20 @@ chart_to_json <- function(x) {
 #' @export
 chart_to_json.list <- function(x) {
   purrr::map(x, chart_to_json) |>
-    jsonlite::toJSON(auto_unbox = TRUE, null = "null", na = "null", json_verbatim = TRUE)
+    to_json(json_verbatim = TRUE)
 }
 
 #' @export
 chart_to_json.highchartzero <- function(x) {
-  purrr::pluck(x, "x", "hc_opts") |>
-    jsonlite::toJSON(auto_unbox = TRUE, null = "null", na = "null")
+  extract_hc_opts(x) |>
+    to_json()
+
+to_json = function(x, auto_unbox = TRUE, null = "null", na = "null", ...) {
+  jsonlite::toJSON(x, auto_unbox = auto_unbox, null = null, na = na, ...)
+}
+
+extract_hc_opts = function(chart_object) {
+  purrr::pluck(chart_object, "x", "hc_opts")
+}
+
 }
