ea6c535b05859d21b5188ba42a8043a33270af48
Eve Perry <eve@eveperry.com>
2023-03-17 10:42:25 -0700

    refactor and comment has_multiple_charts

--- R/utils.R
+++ R/utils.R
@@ -3,5 +3,11 @@ is.highchartzero = function(x) {
 }
 
 has_multiple_charts = function(chart_objects) {
-  !(length(chart_objects) == 1 | is.highchartzero(chart_objects))
+  # single chart if it's a list of length 1 or a highchartzero object itself
+  single_chart =
+    (class(chart_objects) == "list" & length(chart_objects) == 1) |
+    is.highchartzero(chart_objects)
+
+  # it's multiple charts if it's not a single chart
+  !single_chart
 }
